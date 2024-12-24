7d7d72bace5898d6eeb6876e71977f488b1fb23f
tsendebna <tsendbayar.o@ebazaar.mn>
2024-12-24 14:37:25 +0800

    customer health

--- customer/src/routes/health.ts
+++ customer/src/routes/health.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/health", async (req: Request, res: Response) => {
-  res.status(200).json({ status: "service is healthy test" });
+  res.status(200).json({ status: "service is healthy" });
 });
 
 export { router as healthRouter };
